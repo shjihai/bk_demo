c39b810e2c3130e32dcc713fff9b748b37bf26e9
Hai Ji <shjihai@gmail.com>
2018-08-20 08:49:41 +0000

    - mysql config

--- conf/settings_development.py
+++ conf/settings_development.py
@@ -19,10 +19,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
+        'NAME': 'bk_demo_shjihai',                        # 数据库名 (默认与APP_ID相同)
         'USER': 'root',                        # 你的数据库user
-        'PASSWORD': '',                        # 你的数据库password
-        'HOST': '127.0.0.1',                   # 开发的时候，使用localhost
+        'PASSWORD': 'passw0rd',                        # 你的数据库password
+        'HOST': 'mysql.service.consul',                   # 开发的时候，使用localhost
         'PORT': '3306',                        # 默认3306
     },
 }
--- conf/settings_testing.py
+++ conf/settings_testing.py
@@ -19,10 +19,10 @@ from settings import APP_ID
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 默认用mysql
-        'NAME': APP_ID,                        # 数据库名 (默认与APP_ID相同)
-        'USER': '',                            # 你的数据库user
-        'PASSWORD': '',                        # 你的数据库password
-        'HOST': '',                   		   # 数据库HOST
+        'NAME': 'bk_demo_shjihai',                        # 数据库名 (默认与APP_ID相同)
+        'USER': 'root',                            # 你的数据库user
+        'PASSWORD': 'passw0rd',                        # 你的数据库password
+        'HOST': 'mysql.service.consul',                   		   # 数据库HOST
         'PORT': '3306',                        # 默认3306
     },
 }
