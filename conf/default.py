4eea4629044eea2c3a25eefd41d3b0f810a97151
Hai Ji <shjihai@gmail.com>
2018-08-20 10:12:13 +0000

    - fixed api version

--- conf/default.py
+++ conf/default.py
@@ -37,6 +37,7 @@ BK_PAAS_HOST = 'http://paas.demo.com'
 
 # 请求官方 API 默认版本号，可选值为："v2" 或 ""；其中，"v2"表示规范化API，""表示未规范化API
 # DEFAULT_BK_API_VER = 'v2'
+DEFAULT_BK_API_VER = ''
 
 # 是否启用celery任务
 IS_USE_CELERY = True
